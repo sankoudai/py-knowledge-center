ae04ad3f4dd315ccd304114a262515c096cc5a2f
sankoudai <798245503@qq.com>
2021-07-05 23:11:13 +0800

    keras tuner

---
+++ com.xulf.learn.ml.tf2/topic_examples/keras_tuner.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+from tensorflow import keras
+import keras_tuner as kt
+
+# Load data
+(img_train, label_train), (img_test, label_test) = keras.datasets.fashion_mnist.load_data()
+img_train = img_train.astype('float32') / 255.0
+img_test = img_test.astype('float32') / 255.0
+
+# hypermodel
+def model_builder(hp):
+  model = keras.Sequential()
+  model.add(keras.layers.Flatten(input_shape=(28, 28)))
+
+  # Tune the number of units in the first Dense layer
+  # Choose an optimal value between 32-512
+  hp_units = hp.Int('units', min_value=32, max_value=512, step=32)
+  model.add(keras.layers.Dense(units=hp_units, activation='relu'))
+  model.add(keras.layers.Dense(10))
+
+  # Tune the learning rate for the optimizer
+  # Choose an optimal value from 0.01, 0.001, or 0.0001
+  hp_learning_rate = hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])
+
+  model.compile(optimizer=keras.optimizers.Adam(learning_rate=hp_learning_rate),
+                loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=['accuracy'])
+
+  return model
+
+tuner = kt.Hyperband(model_builder,
+                     objective='val_accuracy',
+                     max_epochs=10,
+                     factor=3,
+                     directory='my_dir',
+                     project_name='intro_to_kt')
