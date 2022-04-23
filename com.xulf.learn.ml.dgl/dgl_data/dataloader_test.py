c21394f99757b2cdb58e14d890e38372e1720c93
sankoudai <798245503@qq.com>
2022-04-23 23:59:49 +0800

    dgl: dataloader

--- com.xulf.learn.ml.dgl/dgl_data/dataloader_test.py
+++ com.xulf.learn.ml.dgl/dgl_data/dataloader_test.py
@@ -1,26 +1,63 @@
 import dgl
 import torch
-from dgl.dataloading import NodeDataLoader
+import numpy as np
+from dgl.dataloading import DataLoader,NodeDataLoader, EdgeDataLoader
 from unittest import TestCase
 
 class DataloaderTest(TestCase):
-    def test_node_dataloader(self):
-        '''
-            NodeDataLoader(g, nids, block_sampler, ..)
-            参数：
-                - nids: tensor or {ntype:tensor}, 在graph_sampler中使用
-                - block_sampler: dgl.dataloading.BlockSampler, subgraph sampler
-        '''
-        g = dgl.rand_graph(300, 3000)
-        train_nids = torch.tensor([0, 1, 2, 3, 4, 5])
-        sampler = dgl.dataloading.MultiLayerNeighborSampler([15, 10, 5])
-        dataloader = dgl.dataloading.NodeDataLoader(
-            g, train_nids, sampler,
-            batch_size=3, shuffle=True,  num_workers=1)
-        for input_nodes, output_nodes, blocks in dataloader:
-            print('-----')
-            print(input_nodes)
-            print(output_nodes)
-            print(blocks)
+    '''
+            dgl.dataloading.DataLoader: pytorch DataLoader的子类
+            DataLoader(g, indices,block_sampler, batch_size=1, drop_last=False  ..)
 
-    # def test_edge_dataloader(self):
+            构造参数：
+                - indices: tensor or {type:tensor}, 在graph_sampler中使用
+                - block_sampler: dgl.dataloading.BlockSampler, subgraph sampler
+                (note：
+                    1) indices + batch_size + drop_last => 构造一个节点的dataset
+                    2） g + block_sampler ==> 构造collate_fn
+                    3）两种取值模式：
+                        节点任务：(nids + sampler) => input_nodes, out_nodes, blocks
+                        边任务： (eids + EdgePredictionSampler)=> input_nodes, pos_graph, neg_graph, blocks
+                )
+
+            说明：
+            1）对节点任务， indices
+            EdgeDataLoader(g,
+        '''
+    def test_node_dataloader(self):
+        g = dgl.rand_graph(300, 300)
+
+
+        # 用于节点任务
+        train_nids = torch.tensor([10, 20, 30, 40, 50, 60])
+        sampler = dgl.dataloading.MultiLayerNeighborSampler([15, 10, 5])
+
+        batch_size = 3
+        dataloader = dgl.dataloading.DataLoader(
+            g, train_nids, sampler,
+            batch_size=batch_size, shuffle=False
+        )
+        input_nodes, output_nodes, blocks = next(iter(dataloader))
+        assert len(output_nodes) == batch_size
+        assert blocks[-1].num_dst_nodes() == batch_size
+        assert len(blocks) == 3
+
+        # 用于边任务
+        batch_size, neg_k = 2, 3
+
+        train_eids = torch.tensor([0, 1, 2, 3])
+        neg_sampler = dgl.dataloading.negative_sampler.Uniform(neg_k)
+        node_sampler = dgl.dataloading.NeighborSampler(fanouts=[-1, -1], prob='prob')
+        edge_sampler = dgl.dataloading.EdgePredictionSampler(
+            node_sampler,
+            negative_sampler=neg_sampler,
+            exclude='self')
+
+        dataloader = dgl.dataloading.DataLoader(g, train_eids, edge_sampler, batch_size=batch_size)
+        input_nodes, pos_g, neg_g, blocks = next(iter(dataloader))
+        assert pos_g.num_edges() == batch_size
+        assert neg_g.num_edges() == batch_size * neg_k
+        assert len(blocks) == 2
+
+
+# def test_edge_dataloader(self):
